1810213fa544864bb5c4908df3e42761db53617f
Grigol Taniashvili <147092168+grtaniashvili@users.noreply.github.com>
2025-06-11 11:10:44 +0400

    Update module_3.py

--- Session_3/module_3.py
+++ Session_3/module_3.py
@@ -1,19 +1,29 @@
-# import time
+import time
 from typing import List
 
 Matrix = List[List[int]]
 
 
 def task_1(exp: int):
-    pass
+    def power_factory(n):
+        return n ** exp
+    return power_factory
 
 
 def task_2(*args, **kwags):
-    pass
+    for arg in args:
+        print(arg)
+    for kwarg in kwags.values():
+        print(kwarg)
 
 
 def helper(func):
-    pass
+    def wrapper(*args, **kwargs):
+        print("Hi, friend! What's your name?")
+        result = func(*args, **kwargs)
+        print("See you soon!")
+        return result
+    return wrapper
 
 
 @helper
@@ -22,7 +32,14 @@ def task_3(name: str):
 
 
 def timer(func):
-    pass
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__} in {run_time:.4f} secs")
+        return result
+    return wrapper
 
 
 @timer
@@ -31,8 +48,14 @@ def task_4():
 
 
 def task_5(matrix: Matrix) -> Matrix:
-    pass
+    return [list(row) for row in zip(*matrix)]
 
 
 def task_6(queue: str):
-    pass
+    result = 0
+    for char in queue:
+        if char == '(':
+            result += 1
+        elif char == ')':
+            result -= 1
+    return result == 0
