ef19918c7218dd244d53a2bffa754f9b380d2fac
Grigol Taniashvili <147092168+grtaniashvili@users.noreply.github.com>
2025-06-12 11:10:59 +0400

    Update module_4.py

--- Session_4/module_4.py
+++ Session_4/module_4.py
@@ -3,28 +3,41 @@ PASSING_GRADE = 8
 
 class Trainee:
     def __init__(self, name, surname):
-        pass
+        self.name = name
+        self.surname = surname
+        self.visited_lectures = 0
+        self.done_home_tasks = 0
+        self.missed_lectures = 0
+        self.missed_home_tasks = 0
+        self.mark = 0
 
     def visit_lecture(self):
-        pass
+        self.visited_lectures += 1
+        self._add_points(1)
 
     def do_homework(self):
-        pass
+        self.done_home_tasks += 2
+        self._add_points(2)
 
     def miss_lecture(self):
-        pass
+        self.missed_lectures += 1
+        self._subtract_points(1)
 
     def miss_homework(self):
-        pass
+        self.missed_home_tasks += 2
+        self._subtract_points(2)
 
     def _add_points(self, points: int):
-        pass
+        self.mark = min(self.mark + points, 10)
 
     def _subtract_points(self, points):
-        pass
+        self.mark = max(self.mark - points, 0)
 
     def is_passed(self):
-        pass
+        if self.mark >= PASSING_GRADE:
+            print("Good job!")
+        else:
+            print(f"You need to {PASSING_GRADE - self.mark} points. Try to do your best!")
 
     def __str__(self):
         status = (
